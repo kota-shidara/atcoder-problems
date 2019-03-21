c33a9bf7414e9cd9308f24a75ef15666fb22ac64
kota-shidara <3t.u1.kota@gmail.com>
2019-03-21 22:51:49 +0900

    119-A

---
+++ ABC/119-A.rb
@@ -0,0 +1,15 @@
+# require 'date'
+# date_s = gets
+# date = Date.parse(date_s)
+# if date <= Date.parse("2019/04/30")
+#   puts "Heisei"
+# else
+#   puts "TBD"
+# end
+
+date = gets
+if date <= '2019/05/01'
+  puts "Heisei"
+else
+  puts "TBD"
+end
